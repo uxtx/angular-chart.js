755a0a48a6b9ca0b255156f9f4c25f186aefb7df
Dave Birch <dave@davebir.ch>
2015-11-23 17:05:10 -0600

    making a build

--- dist/angular-chart.js
+++ dist/angular-chart.js
@@ -2,7 +2,9 @@
   'use strict';
   if (typeof exports === 'object') {
     // Node/CommonJS
-    module.exports = factory(typeof angular ? angular : require('angular'), require('chart.js'));
+    module.exports = factory(
+      typeof window.angular ? window.angular : require('angular'),
+      typeof window.Chart ? window.Chart : require('Chart.js'));
   }  else if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['angular', 'chart'], factory);
